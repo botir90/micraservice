fbeef2e964a3f1ba0d83bf395498460c72353d87
botir90 <qodirberganovbotir@gmail.com>
2026-05-25 10:02:06 +0500

    .

--- backend1/src/app.module.ts
+++ backend1/src/app.module.ts
@@ -1,10 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/auth_db'),
+    ConfigModule.forRoot({ isGlobal: true }),
+MongooseModule.forRoot('mongodb://nestuser:nest123@127.0.0.1:27017/auth_db?authSource=admin&authMechanism=SCRAM-SHA-1'),
     AuthModule,
   ],
 })
